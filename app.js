48cc1a637053fdb72f23379285459da7b8f97074
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2021-02-08 19:25:34 +0100

    (feat) add login

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 
 var app = express();
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 600000000 }}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-app.post('/login', function(req, res){
-    console.log(Users);
-    if(!req.body.id || !req.body.password){
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'blog_sport',
+    password: 'nz93dc93',
+    port: 5432,
+});
+
+
+router.get('/',function(req,res){
+    res.render('login')
+});
+
+router.post('/', function(req, res){
+
+    if(!req.body.email || !req.body.password){
         res.render('login', {message: "Please enter both id and password"});
     } else {
-        Users.filter(function(user){
-            if(user.id === req.body.id && user.password === req.body.password){
-                req.session.user = user;
-                res.redirect('/protected_page');
+        let user = {};
+        pool.query("SELECT id FROM users WHERE email = $1 AND password = crypt($2, password)",[req.body.email,req.body.password], (error, results) => {
+            if (error) {
+                throw error
             }
+            if(results.rows){
+                req.session.loggedin = true;
+                res.redirect('/admin' );
+            }
+            res.render('login', {message: "Invalid credentials!"});
+
+
         });
-        res.render('login', {message: "Invalid credentials!"});
     }
 });
+
+module.exports = router;
