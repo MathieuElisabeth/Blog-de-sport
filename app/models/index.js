8a2ee722081dbe83ae052bb8f35853d04aa6dd1e
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2021-02-10 20:49:45 +0100

    (fix) add config files to add more security

--- app/models/index.js
+++ app/models/index.js
@@ -1,8 +1,9 @@
+const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("blog_sport", "postgres", "nz93dc93", {
-    host: "localhost",
-    dialect: "postgres",
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
     operatorsAliases: false,
 });
 
