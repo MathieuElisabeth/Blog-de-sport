010904c2ba0de02af3639d1da9d04bb28b83843a
zuberi <51080597+Nassimzuberi@users.noreply.github.com>
2021-02-12 21:22:44 +0100

    Feature/login (#9)
    * (feat) add login route files
    
    * (feat) add login
    
    * (fix) add username on session
    
    * (fix) add username on session
    
    * (fix) add views on routes
    
    * (feature) add name on input
    
    * (fix) delete index.jade
    
    * (feature) add .env
    
    * (fix) rename login routes
    
    * (feat) add form on front
    
    * (feat) add admin route
    
    * (feat) add admin view
    
    * (fix) redirect to admin if already connect

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/acceuil', { title: 'Express' });
+});
+router.get('/admin', function(req, res, next) {
+  if(req.session.loggedin){
+    res.render('pages/admin');
+  } else {
+    res.redirect('/connexion')
+  }
 });
 
 module.exports = router;
---
+++ routes/login.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+require('dotenv').config()
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+
+router.get('/',function(req,res){
+    if(req.session.loggedin){
+        res.redirect('/admin');
+    }
+    res.render('pages/connexion')
+});
+
+router.post('/', function(req, res){
+
+    if(!req.body.email || !req.body.password){
+        res.render('pages/connexion', {message: "Veuillez entrez votre identifiant et votre mot de passe"});
+    } else {
+        pool.query("SELECT id FROM users WHERE email = $1 AND password = crypt($2, password)",[req.body.email,req.body.password], (error, results) => {
+            if (error) {
+                throw error
+            }
+            if(results.rows.length > 0){
+                req.session.loggedin = true;
+                req.session.username = req.body.email;
+                res.redirect('/admin');
+            }
+            res.render('pages/connexion', {message: "Identifiants incorrects"});
+
+
+        });
+    }
+});
+
+module.exports = router;
