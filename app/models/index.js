5722985a29e5d1414423a9490ae4517e89bdb8b6
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2021-02-12 22:35:20 +0100

    (fix) use .env value on model index

--- app/models/index.js
+++ app/models/index.js
@@ -1,11 +1,12 @@
-const dbConfig = require("../../db.config.js");
+require('dotenv').config()
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     operatorsAliases: false,
 });
+
 
 const db = {};
 
